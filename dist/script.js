e016f57d7d1ee877949e51edede825227ea28ced
iiinaraaa <inaraharu01@gmail.com>
2023-11-14 16:08:49 -0300

    primeiro commit

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+var quantosKm = 0;
+var quantosAnosLuz = 65;
+var quantosKmNumAnoLuz = 9.46e12;
+
+quantosKm = quantosAnosLuz * quantosKmNumAnoLuz;
+quantosKm = quantosKm.toFixed(100);
+alert("Ooi fulaninho  " + "sua resposta é " + quantosKm);
